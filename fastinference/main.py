ffa2747344738c8a6209808a3e42bdd14a81cb4d
Sebastian Buschjäger <sebastian.buschjaeger@tu-dortmund.de>
2021-10-04 22:28:36 +0200

    Using pytorch_lightning container for docs now

--- fastinference/main.py
+++ fastinference/main.py
@@ -103,10 +103,6 @@ def main():
     # to_implementation(loaded_model, args.out_path, args.out_name, weight = 1.0, **unknown)
 
 if __name__ == '__main__':
-    sys.argv = ['fastinference/main.py', '--model', '/tmp/fastinference//DT/DT.json', '--feature_type', 'double', '--out_path', '/tmp/fastinference//DT', '--out_name', 'model', '--implementation', 'cpp.native']
-
-    # sys.argv = ['fastinference/main.py', '--model', '/tmp/fastinference//SimpleMLP/SimpleMLP.onnx', '--feature_type', 'int', '--out_path', '/tmp/fastinference//SimpleMLP', '--out_name', 'model', '--implementation', 'cpp.binary']
     # sys.argv = ['fastinference/main.py', '--model', '/tmp/fastinference//SimpleMLP/SimpleMLP.onnx', '--feature_type', 'double', '--out_path', '/tmp/fastinference//SimpleMLP', '--out_name', 'model', '--implementation', 'fpga.binary']
     print(sys.argv)
     main()
-    # python3 -m fastinference --model /tmp/fastinference/nn/cpp/binary/SimpleMLP.onnx --feature_type int --implementation cpp.NHWC --out_path /tmp/fastinference/nn/cpp/binary/ --out_name "model"
